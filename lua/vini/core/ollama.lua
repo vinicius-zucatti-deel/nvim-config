22adf150991b1d01154449ce91aea8382179c3c3
Vini <viniczb@gmail.com>
2024-06-23 13:14:28 +0200

    fix persistence context bug in ollama integration

--- lua/vini/core/ollama.lua
+++ lua/vini/core/ollama.lua
@@ -1,29 +1,34 @@
--- TODO: the context shold persist
 local chat_context = {}
 
 local chat = function(opts)
   local endpoint = "http://localhost:11434/api/generate"
   local model = "granite-code:3b"
-  local prompt = "CONTEXT: You are an IA assistant with solve math problems. When I ask you to resolve an algorithm, just say the awnser. PROBLEM TO SOLVE:"
+  local prompt = "CONTEXT: You are an IA assistant with solve math problems. When I ask you to resolve an algorithm, just say the awnser. PROBLEM TO SOLVE: "
     .. opts.args
 
-  local json_prompt = vim.fn.json_encode(prompt)
-  -- BUG: We add the context is not reactive
-
-  print("debug:", unpack(chat_context))
+  local json_prompt = vim.fn.json_encode({
+    model = model,
+    prompt = prompt,
+    context = chat_context,
+    stream = false,
+  })
 
   local args = {
     "-s",
     endpoint,
     "-d",
-    string.format('{"model": "%s", "prompt": %s, "stream": false}', model, json_prompt),
+    json_prompt,
   }
+
+  print(vim.inspect(args))
 
   local on_finish = function(result)
     vim.schedule(function()
-      local data = vim.fn.json_decode(result[1])
-      -- BUG: It not update globaly
-      chat_context = data.context
+      local data = vim.fn.json_decode(table.concat(result, ""))
+
+      if data.context then
+        chat_context = data.context
+      end
 
       local chat_response = vim.inspect(data.response)
       vim.cmd("tabnew")
@@ -45,4 +50,6 @@ end
 
 local create_command = vim.api.nvim_create_user_command
 
-create_command("AIChat", chat, { nargs = 1, desc = "Chat with AI" })
+create_command("AIChat", function(opts)
+  chat(opts)
+end, { nargs = 1, desc = "Chat with AI" })
