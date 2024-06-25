2ad413a24ce0e2e36eec21fa30d68eef14a4db23
Vini <viniczb@gmail.com>
2024-06-25 23:38:41 +0200

    add keymaps

--- lua/vini/core/keymaps.lua
+++ lua/vini/core/keymaps.lua
@@ -20,22 +20,47 @@ keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }
 keymap.set("n", "<leader>sm", "<C-W>|", { desc = "Maximaze window" })
 
 -- Tab management
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+keymap.set("n", "<leader>t", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+-- keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+-- keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+-- keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+-- keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+-- keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
 -- text
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste text" })
+
+-- Don't yank on put
+keymap.set(
+  "x",
+  "p",
+  'p<cmd>let @+=@0<CR><cmd>let @"=@0<CR>',
+  { noremap = true, silent = true, desc = "Don't yank on put" }
+)
 keymap.set({ "n", "v" }, "<leader>w", ":w<CR>", { desc = "Save file" })
+keymap.set({ "n", "v" }, "<leader>W", ":w<CR>", { desc = "Save file" })
+
+-- Don't leave visual mode when changing indent
+keymap.set("x", ">", ">gv", { noremap = true })
+keymap.set("x", "<", "<gv", { noremap = true })
 
 -- selection
 keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move up" })
 keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move down" })
+keymap.set({ "n", "x", "o" }, "H", "^", { desc = "Jump to the beginning of the line" })
+keymap.set({ "n", "x", "o" }, "L", "$", { desc = "Jump to the end of the line" })
 keymap.set({ "n", "v" }, "<leader>a", "gg0<C-v>G$", { desc = "Select all" })
 
 -- treesiter
-
 keymap.set("n", "<leader>cti", ":InspectTree<CR>", { desc = "InspectTree" })
+
+-- Clear search highlights easily
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- Efficient buffer navigation
+keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Go to next buffer" })
+keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "Go to previous buffer" })
+
+-- Deleting text in visual mode without copying to clipboard
+keymap.set("x", "d", '"_d', { desc = "Delete without copying" })
