a696fed606469b250a523d45653ce44627779f36
Vini <viniciuszucatti@gmail.com>
2025-05-17 19:56:49 -0300

    Configure diagnostics

--- lua/vini/core/options.lua
+++ lua/vini/core/options.lua
@@ -36,3 +36,16 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
+
+-- FEATURES
+
+vim.diagnostic.config({
+  -- Use the default configuration
+  virtual_lines = true,
+
+  -- Alternatively, customize specific options
+  -- virtual_lines = {
+  --  -- Only show virtual line diagnostics for the current cursor line
+  --  current_line = true,
+  -- },
+})
