e76509cd7b4acb28ba4a206cd689cebe350cd302
Vinicius Zucatti <vinicius.zucatti@deel.com>
2025-10-14 17:47:02 -0300

    Add new colorscheme

---
+++ lua/vini/themes/vscode.lua
@@ -0,0 +1,10 @@
+return {
+  "Mofiqul/vscode.nvim",
+  priority = 1000,
+  lazy = false,
+  -- config = function()
+  --   -- Available styles: 'dark', 'light'
+  --   vim.g.vscode_style = "dark"
+  --   vim.cmd("colorscheme vscode")
+  -- end,
+}
